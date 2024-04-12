d508e8d1ca1670b48151eac4d3b5a099d93d6e3b
Felipe <>
2024-04-11 22:01:10 -0300

    Initial commit

---
+++ src/main/java/br/uni/unibet/UniBet/model/DTO/ResultadoViewDTO.java
@@ -0,0 +1,14 @@
+package br.uni.unibet.UniBet.model.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResultadoViewDTO {
+
+    private String nome;
+    private double valorGanho;
+}
---
+++ src/main/java/br/uni/unibet/UniBet/model/ETipoResultado.java
@@ -0,0 +1,5 @@
+package br.uni.unibet.UniBet.model;
+
+public enum ETipoResultado {
+    AGAUARDANDO, VITORIA_A, VITORIA_B, EMPATE;
+}
